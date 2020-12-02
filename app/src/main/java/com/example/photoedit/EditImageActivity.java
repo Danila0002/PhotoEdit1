e43e88d3c423c64fb4a594292c484d870a105140
Danila0002 <divic086@gmail.com>
2020-12-02 15:35:50 +0500

    https://github.com/Danila0002/PhotoEdit

--- app/src/main/java/com/example/photoedit/EditImageActivity.java
+++ app/src/main/java/com/example/photoedit/EditImageActivity.java
@@ -2,10 +2,16 @@ package com.example.photoedit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class EditImageActivity extends AppCompatActivity {
+    Bitmap selectedimage;
+    ImageView photoView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,21 @@ public class EditImageActivity extends AppCompatActivity {
 
     private void Init() {
         ImageView photo = findViewById(R.id.imageView);
-        photo.setImageBitmap(MainActivity.selectedImage);
+        selectedimage=MainActivity.selectedImage;
+        photoView.setImageBitmap(selectedimage);
+    }
+
+    public void Rotate_90(View view) {
+        Matrix matrix=new Matrix();
+        matrix.setRotate((float)90);
+        selectedimage=Bitmap.createBitmap(selectedimage,0, 0, selectedimage.getWidth(), selectedimage.getHeight(), matrix,false);
+        photoView.setImageBitmap(selectedimage);
+    }
+
+    public void Reflect (View view) {
+        Matrix matrix=new Matrix();
+        matrix.setScale(-1,1);
+        selectedimage=Bitmap.createBitmap(selectedimage,0, 0, selectedimage.getWidth(), selectedimage.getHeight(), matrix,false);
+        photoView.setImageBitmap(selectedimage);
     }
 }
